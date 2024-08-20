b74526a24a2973a1ab91d5053287105227c9b4ca
ritik <ritik@digialpha.co>
2024-08-20 12:38:05 +0530

    flight

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,26 @@
+import NextFederationPlugin from "@module-federation/nextjs-mf";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  webpack(config, options) {
+    const { isServer } = options;
+    const remotes = {
+      host: `host@http://localhost:3000/_next/static/chunks/remoteEntry.js`,
+    };
+    const federatedConfig = {
+      name: "flights",
+      remotes: remotes,
+      shared: {},
+      filename: "static/chunks/remoteEntry.js",
+    };
+    config.plugins.push(
+      new NextFederationPlugin(federatedConfig)
+      // new FederatedTypesPlugin({ federationConfig })
+    );
+    return config;
+  },
 };
-
 export default nextConfig;
